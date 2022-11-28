1d3b00ab26eba353457a73582593c67d99240187
mancinlv <lara.mancinelli@gmail.com>
2022-11-28 09:06:13 -0500

    move interface to domain

--- PaylocityBenefitsCalculator/Api/Application/DependentService.cs
+++ PaylocityBenefitsCalculator/Api/Application/DependentService.cs
@@ -1,29 +1,8 @@
 using Api.Domain.Dependent.Interfaces;
 using Api.Domain.Dependent.Models;
 using Api.Dtos.Dependent;
-
 namespace Application
 {
-    // TODO add CRUD interface w/ generics?
-    //public interface ICRUD
-    //{
-    //    Task<T> GetAsync<T>(int id);
-    //    Task<IList<T>> GetAllAsync<T>();
-    //    Task<IList<T>> AddAsync<T>(T request);
-    //    Task<IList<T>> UpdateAsync<T>(int id, T request);
-    //    Task<IList<T>> DeleteAsync<T>(int id);
-    //}
-
-    public interface IDependentService
-    {
-        Task<GetDependentDto> GetAsync(int id);
-        Task<IList<GetDependentDto>> GetAllAsync();
-        Task<IList<GetDependentDto>> AddAsync(AddDependentWithEmployeeIdDto request);
-        Task<IList<GetDependentDto>> UpdateAsync(int id, UpdateDependentDto request);
-        Task<IList<GetDependentDto>> DeleteAsync(int id);
-        Task<IList<GetDependentDto>> GetAllByEmployeeIdAsync(int employeeId);
-    }
-
     public class DependentService : IDependentService
     {
         private readonly IDependentRepository _dependentRepository;
